ec3c46ec0409b4c966b8d0d69c90bc5751be489f
ilyaryk <ilya.rykovanov@gmail.com>
2024-04-03 11:48:29 +0700

    added_fields_user_id_and_carbodytype

--- bot/db/models/car.py
+++ bot/db/models/car.py
@@ -1,13 +1,28 @@
-from sqlalchemy import Column, String
+import enum
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
 from bot.db.models.base import Base
 
 
+class CarBodyTypes(str, enum.Enum):
+    '''Типы кузовов (надо сделать нормальный список т.к.
+        я в кузовах не разбираюсь)'''
+    type1 = "Тип 1"
+    type2 = "Тип 2"
+    type3 = "Тип 3"
+
+
 class Car(Base):
+    '''Car model'''
     __tablename__ = "car"
-    brand = Column(String, nullable=False)
-    model = Column(String, nullable=False)
-    number = Column(String(6), unique=True)
+    brand: Mapped[str] = mapped_column(String(20), nullable=False)
+    model: Mapped[str] = mapped_column(String(20), nullable=False)
+    number: Mapped[str] = mapped_column(String(20), unique=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    value: Mapped[CarBodyTypes] = mapped_column(default=CarBodyTypes.type1)
 
     def __repr__(self):
         return f"Брэнд-{self.brand}, Модель-{self.model}, Номер-{self.number}"
