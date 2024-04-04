85723e2856b22ef9f4f5dcab6e6147959db39995
ilyaryk <ilya.rykovanov@gmail.com>
2024-04-04 19:03:09 +0700

    alpha_CRUDBase

---
+++ bot/db/crud/base.py
@@ -0,0 +1,82 @@
+from typing import Generic, List, Optional, Type, TypeVar
+
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from bot.db.models.base import Base
+from bot.db.models.user import User
+
+ModelType = TypeVar('ModelType', bound=Base)
+CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
+UpdateSchemaType = TypeVar('UpdateSchemaType', bound=BaseModel)
+
+
+class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+
+    def __init__(
+            self,
+            model: Type[ModelType]
+    ):
+        self.model = model
+
+    async def get(
+            self,
+            session: AsyncSession,
+            obj_id: int,
+    ) -> Optional[ModelType]:
+        db_obj = await session.execute(
+            select(self.model).where(
+                self.model.id == obj_id
+            )
+        )
+        return db_obj.scalars().first()
+
+    async def get_multi(
+            self,
+            session: AsyncSession,
+    ) -> List[ModelType]:
+        db_objs = await session.execute(select(self.model))
+        return db_objs.scalars().all()
+
+    async def create(
+            self,
+            obj_in,
+            session: AsyncSession,
+            user: Optional[User] = None
+    ):
+        obj_in_data = obj_in.dict()
+        if user is not None:
+            obj_in_data['user_id'] = user.id
+        db_obj = self.model(**obj_in_data)
+        session.add(db_obj)
+        await session.commit()
+        await session.refresh(db_obj)
+        return db_obj
+
+    async def update(
+            self,
+            db_obj,
+            obj_in,
+            session: AsyncSession,
+    ):
+        obj_data = jsonable_encoder(db_obj)
+        update_data = obj_in.dict(exclude_unset=True)
+
+        for field in obj_data:
+            if field in update_data:
+                setattr(db_obj, field, update_data[field])
+        session.add(db_obj)
+        await session.commit()
+        await session.refresh(db_obj)
+        return db_obj
+
+    async def remove(
+            self,
+            session: AsyncSession,
+            db_obj: ModelType,
+    ) -> ModelType:
+        await session.delete(db_obj)
+        await session.commit()
+        return db_obj
