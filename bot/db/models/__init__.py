c2a0b939d7ec4a002b51f3c30f42933a180983c5
Anton <anton_ata@mail.ru>
2024-04-04 17:17:03 +0300

    Model fixes and migrations working now

--- bot/db/models/__init__.py
+++ bot/db/models/__init__.py
@@ -1,1 +1,3 @@
+from .base import Base # noqa
+from .car import Car # noqa
 from .payment_transaction import Payment, Visit # noqa
--- bot/db/models/payment_transaction.py
+++ bot/db/models/payment_transaction.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column, relationship, validates
 
 from bot.core.enums import PaymentStateEnum
 from bot.db.models.base import Base
-from bot.db.models.users import User
 
 
 class Payment(Base):
@@ -22,21 +21,29 @@ class Payment(Base):
             raise ValueError("Некорректный статус платежа.")
         return payment_state
 
+    def __repr__(self):
+        return (f"Payment(payment_type_online={self.payment_type_online},"
+                f" payment_state={self.payment_state!r})")
+
 
 class Visit(Base):
     __tablename__ = "visit"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    date: Mapped[datetime]
+    date: Mapped[datetime] = mapped_column(server_default=func.now())
     summ: Mapped[int]
-    bonuspayment: Mapped[bool] = mapped_column(default=False)
-    business_unit_id: Mapped[int] = mapped_column(ForeignKey("business_unit.id"))
+    bonus_payment: Mapped[bool] = mapped_column(default=False)
+    business_unit_id: Mapped[int] = mapped_column(
+        ForeignKey("business_unit.id")
+    )
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     admin_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    admin_user = relationship("User",
-                              foreign_keys=[admin_user_id],
-                              viewonly=True,
-                              primaryjoin="and(Visit.admin_user_id == User.id, User.role == 'admin')")
+    admin_user = relationship(
+        "User",
+        foreign_keys=[admin_user_id],
+        viewonly=True,
+        primaryjoin="and(Visit.admin_user_id == User.id, User.role == 'admin')"
+    )
     car_number: Mapped[str] = mapped_column(ForeignKey("car.number"))
     service_id: Mapped[int] = mapped_column(ForeignKey("services.id"))
     payment_id: Mapped[int] = mapped_column(ForeignKey("payment.id"))
@@ -46,3 +53,5 @@ class Visit(Base):
         if summ < 0:
             raise ValueError("Сумма платежа не может быть отрицательной.")
         return summ
+
+    # TODO: дописать __repr__ по аналогии с payment
