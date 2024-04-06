afac6fa7cf49dcf4e6fa4b7a6ef7a9f96fb810e5
Павел Филиппов <pgphil@yandex.ru>
2024-04-07 01:43:09 +0300

    Correct admin_user in BusinessUnit, correct constants.

--- bot/db/models/business_unit.py
+++ bot/db/models/business_unit.py
@@ -4,8 +4,7 @@ from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column, relationship
 
-from bot.core.constants import (DEFAULT_STRING_SIZE,
-                                MAX_LENGHT_NOTE)
+from bot.core.constants import (DEFAULT_STRING_SIZE, MAX_LENGHT_NOTE)
 from bot.db.models.base import Base
 from bot.db.models.users import User
 
@@ -19,8 +18,7 @@ class BusinessUnit(Base):
     note: Mapped[Optional[str]] = mapped_column(String(MAX_LENGHT_NOTE))
     is_active: Mapped[bool]
     admin_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    admin_user: Optional[User] = relationship(
-        "User",
+    admin_user: Mapped[Optional[User]] = relationship(
         foreign_keys=[admin_user_id],
         viewonly=True,
         primaryjoin="and(BusinessUnit.admin_user_id == User.id, User.role == 'ADMIN')"
