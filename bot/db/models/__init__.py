acb20b3e24ab139bec664143823effff1ab4f036
Vladislav <126501464+Vladislav199912@users.noreply.github.com>
2024-04-07 23:42:51 +0500

    Исправление ошибок

--- bot/db/models/__init__.py
+++ bot/db/models/__init__.py
@@ -1,7 +1,10 @@
-from .base import Base  # noqa
-from .bonus_model import (BonusCase, BonusesBatch, BonusPayment,  # noqa
-                          BonusTime)
-from .car import Car  # noqa
-from .payment_transaction import Payment, Visit  # noqa
-from .services import Service # noqa
+from .base import Base # noqa
+from .business_unit import BusinessUnit # noqa
+from .car import Car # noqa
+from .payment_transaction import Payment, Visit # noqa
 from .users import User  # noqa
+from .bonus_model import (
+    BonusCase, BonusesBatch, BonusPayment,
+    BonusTime,
+) # noqa
+from .services import Service # noqa
