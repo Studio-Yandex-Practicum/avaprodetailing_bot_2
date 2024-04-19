01dad8060181ffafc3eb488e70845fb687050f61
Vladislav <126501464+Vladislav199912@users.noreply.github.com>
2024-04-19 18:16:57 +0500

    V2.0

--- bot/handlers/user/__init__.py
+++ bot/handlers/user/__init__.py
@@ -1,10 +1,6 @@
 from aiogram import Router
 
-<<<<<<< HEAD
 from . import registration, start, profile, services
-=======
-from . import profile, registration, start
->>>>>>> origin/develop
 
 user_router = Router(name=__name__)
 user_router.include_routers(start.router, registration.router, profile.router, services.router)
