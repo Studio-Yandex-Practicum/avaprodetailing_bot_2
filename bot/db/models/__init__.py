2f341a0562e75b7cf4ef18521d6295ed71965ce3
Anton Zemtsov <anton_ata@mail.ru>
2024-04-22 02:45:53 +0300

    Revert "Correct visit, and payment handler."

--- bot/db/models/__init__.py
+++ bot/db/models/__init__.py
@@ -2,6 +2,6 @@ from .base import Base  # noqa
 from .bonus import Bonus, BonusCase  # noqa
 from .business_unit import BusinessUnit  # noqa
 from .car import Car  # noqa
-from .visit import Visit  # noqa
+from .payment_transaction import Visit  # noqa
 from .services import Service, ServiceCategory, ServiceUnit  # noqa
 from .users import User  # noqa
--- bot/db/models/payment_transaction.py
+++ bot/db/models/payment_transaction.py
@@ -1,10 +1,10 @@
 from datetime import datetime
+from typing import Optional
 
 from sqlalchemy import CheckConstraint, ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from bot.core.enums import PaymentState
-from bot.core.constants import PAID
 from bot.db.models.base import Base
 
 
@@ -16,9 +16,12 @@ class Visit(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     date: Mapped[datetime] = mapped_column(server_default=func.now())
-    summ: Mapped[int]
-    payment_type_online: Mapped[bool] = mapped_column(default=False)
-    payment_state: Mapped[PaymentState] = mapped_column(default=PAID)
+    summ: Mapped[int] = mapped_column(
+        CheckConstraint("summ >= 0", name='check_summ_positive')
+    )
+    bonus_payment: Mapped[bool] = mapped_column(default=False)
+    payment_type_online: Mapped[bool]
+    payment_state: Mapped[PaymentState]
     business_unit_id: Mapped[int] = mapped_column(
         ForeignKey('business_units.id')
     )
@@ -32,7 +35,9 @@ class Visit(Base):
         foreign_keys=(admin_user_id,)
     )
     car_id: Mapped[str] = mapped_column(ForeignKey('cars.id'))
-    services: Mapped[str]
+    service_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey('services.id')
+    )
 
     def __repr__(self):
         return (f'Visit(id={self.id}, summ={self.summ},'
@@ -41,11 +46,9 @@ class Visit(Base):
     @classmethod
     def data_to_model(cls, obj_in):
         db_obj = cls(
-            summ=obj_in['payment_amount'],
+            summ=obj_in['amount'],
             business_unit_id=obj_in['business_unit_id'],
             user_id=obj_in['user_id'],
             admin_user_id=obj_in['admin_user_id'],
-            car_id=obj_in['car'].id,
-            services=obj_in['chosen_services']
         )
         return db_obj
